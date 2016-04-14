589576af825fedfe70cabc3333f531d2036c1bc9
HC <chunhuang@zdongpin.com>
2016-04-14 11:57:52 +0800

    add gulp config

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var path = require('path'),
+	gulp = require('gulp'),
+	bs = require('browser-sync').create(),
+	plumber = require('gulp-plumber'),
+	less = require('gulp-less'),
+	sourceMap = require('gulp-sourcemaps');
+
+var PUBLIC = './src/public/',
+	STYLE = PUBLIC + 'style/',
+	LESS = [STYLE + 'less/**/*.less'],
+	CSS = [STYLE + 'css/*.css'],
+	PAGES = ['./src/views/**/*.html'];
+
+gulp.task('less', function () {
+	return gulp.src(LESS)
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(sourceMap.write())
+		.pipe(gulp.dest(STYLE + '/css'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch(PAGES).on('change', function (e) {
+		console.log(e.path + ' changed...');
+		bs.reload()
+	});
+	gulp.watch(LESS, ['less']);
+	
+	gulp.watch(CSS).on('change', function (e) {
+		console.log(e.path + ' changed...')
+		bs.reload(e.path);
+	});
+});
+
+gulp.task('default', ['less'], function () {
+	bs.init({
+		server: './src'
+	});
+	gulp.run('watch');
+});
