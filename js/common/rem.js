fea9d5619528a3e6171af66a33ad64c884350139
HC <chunhuang@zdongpin.com>
2016-04-14 10:41:39 +0800

    project init

---
+++ js/common/rem.js
@@ -0,0 +1,14 @@
+(function (doc,win) {
+    // 获取根html元素
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize';
+    var recalc = function () {
+	        var clientWidth = docEl.clientWidth;
+	        if ( !clientWidth ) return;
+	        docEl.style.fontSize = ( clientWidth / 375 ) * 16 + 'px';
+	    };
+    if ( !doc.addEventListener ) return;
+    win.addEventListener( resizeEvt , recalc , false );
+    doc.addEventListener( 'DOMContentLoaded' , recalc , false );
+    recalc();
+})(document,window);
