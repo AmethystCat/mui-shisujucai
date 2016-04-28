c188f102a45cef1511b582561445d4cc2d223869
HC <chunhuang@zdongpin.com>
2016-04-28 18:32:13 +0800

    developing

--- src/public/js/me/main.js
+++ src/public/js/me/main.js
@@ -1,6 +1,15 @@
+var rootPath = '/src/views/'
 mui.init({
 	styles: {
 		top: 0,
 		bottom: '50px'
 	}
-})
+});
+mui.ready(function () {
+	mui('.me-info-w').on('click', '.name', function () {
+		mui.openWindow({
+			url: rootPath + 'me/info.html',
+			id: rootPath + 'me/info.html'
+		});
+	});
+});
